5739eba17ef2b449f104dc835d86ca1b3fd2e74d
Marvelous rose <marvelousrhose@gmail.com>
2022-10-20 17:42:07 +0100

    updates

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const pension_code = document.getElementById('pension_code');
 const lists = document.getElementById('lists');
 const employee_code = document.getElementById('employee_code');
 const upload_file = document.getElementById('upload-file');
+const pension_company = document.getElementById('pension_company');
 
 
 function reset(input) {
@@ -47,13 +48,26 @@ myform.addEventListener('submit', (e) => {
 
 function checkInputs(e) {
   const pension_code = pension_code.value.trim();
+  const employee_code = employee_code.value.trim();
+  const lists = lists.value.trim();
+  const pension_company = pension_company.value.trim();
+  const upload_file = upload_file.value.trim();
+
   if (pension_code === '') {
     setErrorFor(pension_code, ' Please fill out this field');
     e.preventDefault();
   } else {
     setSuccessFor(pension_code);
   }
+
+
+if (pension_company === '') {
+  setErrorFor(pension_company, ' Please select a pension company');
+  e.preventDefault();
+} else {
+  setSuccessFor(pension_company);
 }
+
 
 if (lists === '') {
     setErrorFor(lists, ' Please fill out this field');
@@ -76,7 +90,7 @@ if (lists === '') {
     setSuccessFor(upload_file);
   }
 
-
+}
 
 function setErrorFor(input, message) {
     const input_wrap = input.input_wrap;
@@ -93,18 +107,19 @@ function setSuccessFor(input) {
   input_wrap.classList.remove('error');
 }
 
-// email validating
+
 
 
 pension_code.addEventListener('keyup', function () {
   reset(pension_code);
 });
+
 lists.addEventListener('keyup', function () {
     reset(lists);
   });
-  employee_code.addEventListener('keyup', function () {
-    reset(employee_code);
-  });
-  upload_file.addEventListener('keyup', function () {
-    reset(upload_file);
-  });
+
+employee_code.addEventListener('keyup', function () {
+   reset(employee_code);
+ });
+
+   
