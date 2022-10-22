4d8c2bec1c60b10a0e6c74f94c51c46aced36238
Ifypro <ifesinachi3992@gmail.com>
2022-10-22 12:43:44 -0400

    added validation to input fields

--- index.js
+++ index.js
@@ -42,8 +42,7 @@ const pension_code = document.getElementById('pension_code');
 const lists = document.getElementById('lists');
 const company_list = document.getElementById('company_list')
 const employee_code = document.getElementById('employee_code');
-const upload_file = document.getElementById('upload_file');
-
+const upload_file = document.getElementById('upload-file');
 
 
 function reset(input) {
@@ -59,29 +58,21 @@ myform.addEventListener('submit', (e) => {
 });
 
 function checkInputs(e) {
-  const pCode = pension_code.value.trim();
-  if (pCode === '') {
+  const pension_code = pension_code.value.trim();
+  if (pension_code === '') {
+    setErrorFor(pension_code, ' Please fill out this field');
     e.preventDefault();
     setErrorFor(pension_code, 'Please fill out this field');
   } else {
     setSuccessFor(pension_code);
   }
+}
 
-
-  if (lists.value === '') {
+if (lists === '') {
+    setErrorFor(lists, ' Please fill out this field');
     e.preventDefault();
-    setErrorFor(lists, 'Please fill out this field');
   } else {
     setSuccessFor(lists);
-  }
-
-  if(!document.getElementById('pull').getAttribute('class').includes('showhide')){
-    if (company_list.value === '') {
-      e.preventDefault();
-      setErrorFor(company_list, 'Please fill out this field');
-    } else {
-      setSuccessFor(company_list);
-    }
   }
 
   if(!document.getElementById('yes_output').getAttribute('class').includes('showhide')){
@@ -103,7 +94,7 @@ function checkInputs(e) {
     }
   }
 
-}
+
 
 function setErrorFor(input, message) {
   const input_wrap = input.parentElement;
@@ -120,18 +111,17 @@ function setErrorFor(input, message) {
 }
 
 // email validating
+
+
 pension_code.addEventListener('keyup', function () {
   reset(pension_code);
 });
-lists.addEventListener('change', function () {
-  reset(lists);
-});
-company_list.addEventListener('change', function () {
-  reset(company_list);
-});
-employee_code.addEventListener('keyup', function () {
-  reset(employee_code);
-});
-upload_file?.addEventListener('change', function () {
-  reset(upload_file);
-});
+lists.addEventListener('keyup', function () {
+    reset(lists);
+  });
+  employee_code.addEventListener('keyup', function () {
+    reset(employee_code);
+  });
+  upload_file.addEventListener('keyup', function () {
+    reset(upload_file);
+  });
